c1e94da67dd4114c70a4fc080577148aefefc690
liasica <magicrolan@qq.com>
2022-11-13 21:43:33 +0800

    代码更新

--- internal/core/kaixin/kaixin.go
+++ internal/core/kaixin/kaixin.go
@@ -60,10 +60,28 @@ type LoginRequest struct {
     ProtocolVersion string `json:"protocolVersion,omitempty"` // 协议版本
 }
 
+// ReportCate 上报类型
+type ReportCate int
+
+const (
+    ReportCateIncremental ReportCate = iota // 增量
+    ReportCateFull                          // 全量
+)
+
+func (c ReportCate) String() string {
+    switch c {
+    case ReportCateIncremental:
+        return "增量"
+    case ReportCateFull:
+        return "全量"
+    }
+    return ""
+}
+
 // ReportRequest 上报电柜属性
 type ReportRequest struct {
-    AttrList []Attr `json:"attrList,omitempty"` // 属性列表
-    IsFull   *int   `json:"isFull,omitempty"`   // 是否全量上报: 0:增量 1:全量
+    AttrList []Attr     `json:"attrList,omitempty"` // 属性列表
+    IsFull   ReportCate `json:"isFull,omitempty"`   // 是否全量上报: 0:增量 1:全量
 }
 
 // Attr 属性信息
@@ -77,15 +95,33 @@ type NoticeRequest struct {
     AlarmList []Alarm `json:"alarmList,omitempty"` // 告警列表
 }
 
+// AlarmFlag 告警标识
+type AlarmFlag int
+
+const (
+    AlarmFlagEnd AlarmFlag = iota
+    AlarmFlagStart
+)
+
+func (a AlarmFlag) String() string {
+    switch a {
+    case AlarmFlagEnd:
+        return "结束"
+    case AlarmFlagStart:
+        return "开始"
+    }
+    return ""
+}
+
 // Alarm 告警属性
 type Alarm struct {
-    ID         string `json:"id,omitempty"`         // 信号量ID
-    AlarmTime  int64  `json:"alarmTime,omitempty"`  // 告警时间: 13位时间戳
-    AlarmmDesc string `json:"alarmmDesc,omitempty"` // 描述 (可为空)
-    AlarmFlag  *int   `json:"alarmFlag,omitempty"`  // 告警标识: 0:结束 1:开始
-    DoorID     string `json:"doorId,omitempty"`     // 柜门ID (可为空, 对应信号量类别属于电池、柜门，字段必填)
-    BatteryID  string `json:"batteryId,omitempty"`  // 电池ID (可为空, 对应信号量类别属于电池，字段必填)
-    UserID     string `json:"userId,omitempty"`     // App用户ID (可为空, 首放、换电、退电的解绑、绑定事件必填)
+    ID         string    `json:"id,omitempty"`         // 信号量ID
+    AlarmTime  int64     `json:"alarmTime,omitempty"`  // 告警时间: 13位时间戳
+    AlarmmDesc string    `json:"alarmmDesc,omitempty"` // 描述 (可为空)
+    AlarmFlag  AlarmFlag `json:"alarmFlag,omitempty"`  // 告警标识: 0:结束 1:开始
+    DoorID     string    `json:"doorId,omitempty"`     // 柜门ID (可为空, 对应信号量类别属于电池、柜门，字段必填)
+    BatteryID  string    `json:"batteryId,omitempty"`  // 电池ID (可为空, 对应信号量类别属于电池，字段必填)
+    UserID     string    `json:"userId,omitempty"`     // App用户ID (可为空, 首放、换电、退电的解绑、绑定事件必填)
 }
 
 // ControlRequest 下发控制请求
@@ -103,7 +139,7 @@ var (
 // ControlParam 控制参数
 type ControlParam struct {
     SignalData
-    DoorID      string      `json:"doorId,omitempty"`      // 柜门ID (可为空, 如果控制的是柜门，字段必填，否则可以省略该字段)
+    DoorID      string      `json:"doorId,omitempty"`      // 柜门ID (从1开始, 可为空, 如果控制的是柜门，字段必填，否则可以省略该字段)
     BatteryID   string      `json:"batteryId,omitempty"`   // 电池设备ID (可为空, 如果控制的是电池，字段必填，否则可以省略该字段)
     Voltage     string      `json:"voltage,omitempty"`     // 电池电压 (可为空, 首放流程中这个字段必填)
     ScanBattery ScanBattery `json:"scanBattery,omitempty"` // 电池是否支持通讯 (可为空, 0:支持 1:不支持，扫换电柜这个字段必填)
---
+++ internal/core/type.go
@@ -0,0 +1,8 @@
+// Copyright (C) liasica. 2022-present.
+//
+// Created at 2022-11-12
+// Based on cabservd by liasica, magicrolan@qq.com.
+
+package core
+
+type PointInt *int
