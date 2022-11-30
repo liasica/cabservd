795b8c6698e798d977c79954ab42315471bbefd6
liasica <magicrolan@qq.com>
2022-11-30 12:33:48 +0800

    service

---
+++ service/control.go
@@ -0,0 +1,10 @@
+// Copyright (C) liasica. 2022-present.
+//
+// Created at 2022-11-30
+// Based on cabservd by liasica, magicrolan@qq.com.
+
+package service
+
+func OpenDoor(brand, serial string, param any) (err error) {
+    return
+}
---
+++ service/task.go
@@ -0,0 +1,6 @@
+// Copyright (C) liasica. 2022-present.
+//
+// Created at 2022-11-30
+// Based on cabservd by liasica, magicrolan@qq.com.
+
+package service
