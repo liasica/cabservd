3ef6748bcec576c8f769946c7fe2985306b4998a
liasica <magicrolan@qq.com>
2023-01-06 18:15:41 +0800

    自动维护

--- internal/controller/api/maintain.go
+++ internal/controller/api/maintain.go
@@ -6,15 +6,26 @@
 package api
 
 import (
-    "github.com/auroraride/cabservd/internal/service"
+    "github.com/auroraride/cabservd/internal/g"
+    "github.com/auroraride/cabservd/internal/task"
     "github.com/labstack/echo/v4"
+    "time"
 )
 
 type maintain struct{}
 
 var Maintain = new(maintain)
 
-func (*maintain) Update(c echo.Context) (err error) {
-    service.NewMaintain().Update()
+func (*maintain) Update(echo.Context) (err error) {
+    ticker := time.NewTicker(time.Second)
+    defer ticker.Stop()
+
+    for ; true; <-ticker.C {
+        n := task.Bin.GetListenerCount() + task.Cabinet.GetListenerCount()
+        if n == 0 {
+            g.Quit <- true
+            return
+        }
+    }
     return nil
 }
