fbe819637bd6090310f9fd68e303ae36e15654e0
liasica <magicrolan@qq.com>
2023-03-04 08:50:49 +0800

    rpc备份

---
+++ internal/rpc/rpc.go
@@ -0,0 +1,26 @@
+// Copyright (C) liasica. 2023-present.
+//
+// Created at 2023-03-01
+// Based on cabservd by liasica, magicrolan@qq.com.
+
+package rpc
+
+func Run() {
+    // lis, err := net.Listen("tcp", g.Config.Rpc.Bind)
+    // if err != nil {
+    //     zap.L().Fatal("RPC启动失败", zap.Error(err))
+    //     return
+    // }
+    //
+    // s := grpc.NewServer(
+    //     // TODO DUMP MIDDLEWARE
+    // )
+    // pb.RegisterCabinetServer(
+    //     s,
+    //     &cabinetServer{},
+    // )
+    // err = s.Serve(lis)
+    // if err != nil {
+    //     zap.L().Fatal("RPC启动失败", zap.Error(err))
+    // }
+}
