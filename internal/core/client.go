aa188ef3e3010a8073047bc9b57d175cc3c6919a
liasica <magicrolan@qq.com>
2023-05-10 12:04:54 +0800

    删除无用代码

--- internal/core/client.go
+++ internal/core/client.go
@@ -9,12 +9,13 @@ import (
 	"context"
 	"time"
 
+	"github.com/panjf2000/gnet/v2"
+	"go.uber.org/zap"
+
 	"github.com/auroraride/cabservd/internal/codec"
 	"github.com/auroraride/cabservd/internal/ent"
 	"github.com/auroraride/cabservd/internal/ent/cabinet"
 	"github.com/auroraride/cabservd/internal/g"
-	"github.com/panjf2000/gnet/v2"
-	"go.uber.org/zap"
 )
 
 type Client struct {
@@ -51,45 +52,6 @@ type ResponseMessenger interface {
 
 // SendMessage 向客户端发送消息
 func (c *Client) SendMessage(messenger ResponseMessenger) (err error) {
-	// var (
-	//     b      []byte
-	//     data   []byte
-	//     fields []zap.Field
-	//     useHex bool
-	// )
-	//
-	//
-	// switch v := message.(type) {
-	// case []byte:
-	//     b = v
-	//     useHex = true
-	// default:
-	//     b, _ = jsoniter.Marshal(message)
-	// }
-	//
-	// defer func() {
-	//     for _, opt := range options {
-	//         switch ov := opt.(type) {
-	//         case zap.Field:
-	//             fields = append(fields, ov)
-	//         }
-	//     }
-	//     if useHex {
-	//         fields = append(fields, log.Hex(data))
-	//     } else {
-	//         fields = append(fields, log.ResponseBody(b))
-	//     }
-	//     level := zap.InfoLevel
-	//     if err != nil {
-	//         level = zap.ErrorLevel
-	//         fields = append(fields, zap.Error(err), log.Binary(data))
-	//     }
-	//     c.Log(level, "发送消息 ↓ ", fields...)
-	// }()
-	//
-	// data = c.Hub.codec.Encode(b)
-	//
-	// _, err = c.Write(data)
 	b, fields := messenger.GetMessage(c.Hub.codec)
 	_, err = c.Write(b)
 	lvl := zap.InfoLevel
